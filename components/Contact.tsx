22b6f8edec999268d2df3945a080cc856f98f1d6
Thiago Maués <thiago.mauess@gmail.com>
2026-01-21 15:57:44 -0300

    feat:updated map visuals

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -23,7 +23,7 @@ const Contact: React.FC = () => {
          <div className="absolute top-0 left-0 w-2 h-2 bg-[#ccff00] z-20"></div>
          <div className="absolute bottom-0 right-0 w-2 h-2 bg-[#ccff00] z-20"></div>
 
-         <div className="w-full h-[300px] bg-[#121212] overflow-hidden filter grayscale contrast-125 invert-[.1]">
+         <div className="w-full h-[300px] bg-[#121212] overflow-hidden filter grayscale invert brightness-90 contrast-125">
           <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127637.3697960134!2d-48.4901799!3d-1.4557549!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x92a48bb316138677%3A0x67a33903173d120a!2sBel%C3%A9m%2C%20PA!5e0!3m2!1spt-BR!2sbr!4v1647608789441!5m2!1spt-BR!2sbr" 
             width="100%" 
